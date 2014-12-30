283f95997dfa13c7805c1771a59b597f8252dbfd
Nicolas DURAN <duran_n@epita.fr>
2014-12-30 13:54:32 +0100

    debug lecture image

--- img.cpp
+++ img.cpp
@@ -1,9 +1,15 @@
 #include "img.hpp"
+#include <iostream>
 
 Img::Img(std::string path)
 {
 	cv::Mat image = cv::imread(path, CV_LOAD_IMAGE_COLOR);
     	if(image.empty()) 
-       		return -1;
+	{       		
+		std::cout << "Error opening image : ";
+		std::cout << path;
+		std::cout << std::endl;
+		exit(4);
+	}
 	this->img = image;
 }
