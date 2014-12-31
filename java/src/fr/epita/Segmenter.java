7a8eba066e2d57d9e84af1188208adbf4a3e4ce5
NicolasD <neowa.gm@gmail.com>
2014-12-31 13:32:06 +0100

    aha

--- java/src/fr/epita/Segmenter.java
+++ java/src/fr/epita/Segmenter.java
@@ -17,24 +17,24 @@ public class Segmenter {
 	{
 		int img_width = image.getWidth();
 		int img_height = image.getHeight();
-		// create clusters
+
 		colorGroups = makeGroups(image, nb_of_class);
-		// create cluster lookup table
 		int[] cgroupsTable = new int[img_width * img_height];
 		Arrays.fill(cgroupsTable, -1);
 
-		// at first loop all pixels will move their clusters
 		boolean changed = true;
-		// loop until all clusters are stable!
-
 		while (changed) 
 		{
 			changed = false;
-			for (int y = 0; y < img_height; y++) {
-				for (int x = 0; x < img_width; x++) {
+			for (int y = 0; y < img_height; y++) 
+			{
+				for (int x = 0; x < img_width; x++) 
+				{
 					int pixel = image.getRGB(x, y);
 					ColorGroup cluster = getBestGroup(pixel);
-					if (cgroupsTable[img_width * y + x] != cluster.getId()) {
+					
+					if (cgroupsTable[img_width * y + x] != cluster.getId()) 
+					{
 						changed = true;
 						cgroupsTable[img_width * y + x] = cluster.getId();
 					}
@@ -43,27 +43,30 @@ public class Segmenter {
 			for (int i = 0; i < colorGroups.size(); i++) {
 				colorGroups.get(i).reset();
 			}
-			for (int y = 0; y < img_height; y++) {
-				for (int x = 0; x < img_width; x++) {
-					int id = cgroupsTable[img_width * y + x];
-					// add pixels to cluster
+			for (int y = 0; y < img_height; y++) 
+			{
+				for (int x = 0; x < img_width; x++) 
+				{
+					int id = cgroupsTable[(img_width * y) + x];
 					colorGroups.get(id).addPixel(image.getRGB(x, y));
 				}
 			}
 
 		}
-		// create result image
-		BufferedImage result = new BufferedImage(img_width, img_height,
-				BufferedImage.TYPE_INT_RGB);
-		for (int y = 0; y < img_height; y++) {
-			for (int x = 0; x < img_width; x++) {
-				int id = cgroupsTable[img_width * y + x];
-				result.setRGB(x, y, colorGroups.get(id).colorOfCluster());
+
+		BufferedImage image_out = new BufferedImage(img_width, img_height, BufferedImage.TYPE_INT_RGB);
+		for (int y = 0; y < img_height; y++) 
+		{
+			for (int x = 0; x < img_width; x++) 
+			{
+				int id = cgroupsTable[(img_width * y) + x];
+				image_out.setRGB(x, y, colorGroups.get(id).colorOfCluster());
 			}
 		}
-		return result;
+		return image_out;
 	}
-	//Ajouter du randomm?
+	
+	//Ajouter du random?
 	public ArrayList<ColorGroup> makeGroups(BufferedImage image, int nb_of_class) 
 	{
 		ArrayList<ColorGroup> result = new ArrayList<ColorGroup>(nb_of_class);
@@ -81,7 +84,9 @@ public class Segmenter {
 
 	public ColorGroup getBestGroup(int rgb) {
 		ColorGroup groups = null;
+		
 		int min = Integer.MAX_VALUE;
+		
 		for (int i = 0; i < colorGroups.size(); i++) 
 		{
 			int dist = colorGroups.get(i).chromaticDistance(rgb);
